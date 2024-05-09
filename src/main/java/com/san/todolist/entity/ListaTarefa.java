1bf4666baa0492f72bea141de1aa8d1fa57df473
Alessandro Menezes <alessandromenezes22@gmail.com>
2024-05-08 23:59:38 -0300

    Adicionado anotações do jpa nas entity

--- src/main/java/com/san/todolist/entity/ListaTarefa.java
+++ src/main/java/com/san/todolist/entity/ListaTarefa.java
@@ -3,6 +3,13 @@ package com.san.todolist.entity;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,12 +19,22 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "lista_tarefa")
 public class ListaTarefa {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String titulo;
+
     private String descricao;
+
     private LocalDateTime dataCriacao;
+
     private LocalDateTime dataAlteracao;
+
+    @OneToMany(mappedBy = "", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Tarefa> tarefas;
 }
--- src/main/java/com/san/todolist/entity/Tarefa.java
+++ src/main/java/com/san/todolist/entity/Tarefa.java
@@ -2,6 +2,12 @@ package com.san.todolist.entity;
 
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,11 +17,22 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "tarefa")
 public class Tarefa {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String nome;
+
     private Status status;
+
     private LocalDateTime dataCriacao;
+
     private LocalDateTime dataAlteracao;
+
+    @ManyToOne
+    private ListaTarefa listaTarefa;
 }
